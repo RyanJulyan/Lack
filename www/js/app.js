9fb1416e43fc80570a0bb2541cc2abf98e8b7cd5
ryan.julyan <ryan@julyan.biz>
2019-08-24 10:35:39 +0200

    Updated background things for example

--- www/js/app.js
+++ www/js/app.js
@@ -87,13 +87,30 @@ var errorCallback = function(e) {
   app.dialog.alert('error: ' + e);
 };
 
+// document.addEventListener('deviceready', function () {
+    // // cordova.plugins.backgroundMode is now available
+	// cordova.plugins.backgroundMode.enable();
+	// var backgroundModeActive = cordova.plugins.backgroundMode.isActive();
+		
+	// if(backgroundModeActive){
+		// cordova.plugins.backgroundMode.unlock();
+	// }
+		
+// }, false);
+
 document.addEventListener('deviceready', function () {
-    // cordova.plugins.backgroundMode is now available
-	cordova.plugins.backgroundMode.enable();
-	var backgroundModeActive = cordova.plugins.backgroundMode.isActive();
-		
-	if(backgroundModeActive){
-		cordova.plugins.backgroundMode.unlock();
-	}
-		
+    // Android customization
+    cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
+    // Enable background mode
+    cordova.plugins.backgroundMode.enable();
+
+    // Called when background mode has been activated
+    cordova.plugins.backgroundMode.onactivate = function () {
+        setTimeout(function () {
+            // Modify the currently displayed notification
+            cordova.plugins.backgroundMode.configure({
+                text:'Running in background for more than 5s now.'
+            });
+        }, 5000);
+    }
 }, false);
