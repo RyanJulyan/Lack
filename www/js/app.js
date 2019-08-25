7a479c2b76a86b2b7bbb2c8a5008575f8eab4f18
ryan.julyan <ryan@julyan.biz>
2019-08-25 21:01:09 +0200

    Updated for social sharing

--- www/js/app.js
+++ www/js/app.js
@@ -29,7 +29,25 @@ var app = new Framework7({
         app.dialog.alert('More');
       },
       share: function () {
-        app.dialog.alert('Share');
+        var options = {
+			message: 'share this', // not supported on some apps (Facebook, Instagram)
+			subject: 'the subject', // fi. for email
+			files: ['', ''], // an array of filenames either locally or remotely
+			url: 'https://www.website.com/foo/#bar?a=b',
+			chooserTitle: 'Pick an app', // Android only, you can override the default share sheet title,
+			appPackageName: 'com.apple.social.facebook' // Android only, you can provide id of the App you want to share with
+		};
+
+		var onSuccess = function(result) {
+			console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+			console.log("Shared to app: " + result.app); // On Android result.app since plugin version 5.4.0 this is no longer empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+		};
+
+		var onError = function(msg) {
+			console.log("Sharing failed with message: " + msg);
+		};
+
+		window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
       },
   },
 	on: {
@@ -52,6 +70,9 @@ var app = new Framework7({
 		});
 		$(document).on("click", "#more", function(){
 			app.methods.more();
+		});
+		$(document).on("click", ".share", function(){
+			app.methods.share();
 		});
 		$(document).on("click", "#share", function(){
 			app.methods.share();
